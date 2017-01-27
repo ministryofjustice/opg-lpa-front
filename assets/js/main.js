4b868126404425a1c3d71396366d69f8f90265f2
Max Edwards <youwontforgetthis@gmail.com>
2017-01-27 17:57:37 +0000

    Fixed a lot of the broken visuals and missing fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,12 @@ var $blockLabels = $(".block-label input[type='radio'], .block-label input[type=
 new GOVUK.SelectionButtons($blockLabels);
 
 
+// Where .block-label uses the data-target attribute
+// to toggle hidden content
+var showHideContent = new GOVUK.ShowHideContent();
+showHideContent.init();
+
+
 // ====================================================================================
 // VENDOR CONFIGURATION
 
